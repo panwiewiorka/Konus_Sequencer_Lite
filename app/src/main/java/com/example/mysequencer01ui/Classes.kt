8b7c9f7c0bd65ae1cfb7ae6f89a8ce14bc20a1e8
Konstantin Stakhurskii <konst.stakh@gmail.com>
2023-05-19 14:30:31 +0200

    Draft layout: Clear, Mute, Del Rec, Play, Stop status_text, All P A D S _______ Before splitting NOTES_list into separate channels

--- app/src/main/java/com/example/mysequencer01ui/Classes.kt
+++ app/src/main/java/com/example/mysequencer01ui/Classes.kt
@@ -1,8 +1,8 @@
 package com.example.mysequencer01ui
 
 data class Note (
+    var time: Long,
+    var channel: Int = 0,
     var pitch: Int = 60,
     var velocity: Int = 100,
-    var startTime: Long = 0L,
-    var length: Long = 100L,
         )
